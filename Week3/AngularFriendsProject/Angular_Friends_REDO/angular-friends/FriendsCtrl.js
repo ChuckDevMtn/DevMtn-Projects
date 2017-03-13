26fa9a2fa2ba65436fcf6a2af687e90891fc8ec8
ChuckDevMtn <Chuck.DevMtn@gmail.com>
2017-03-12 19:57:23 -0600

    project addition

--- Week3/AngularFriendsProject/Angular_Friends_REDO/angular-friends/FriendsCtrl.js
+++ Week3/AngularFriendsProject/Angular_Friends_REDO/angular-friends/FriendsCtrl.js
@@ -1,23 +1,18 @@
-angular.module('myApp').controller('FriendCtrl', function($scope){
-   //  $scope.test = 'Hello World';
+angular.module('FriendsApp').controller('FriendsCtrl', function($scope){
 
-$scope.sortProp;
-$scope.sortDirection;
+// $scope.test = "Hello";
 
-
-
-
-$scope.friends = [{
+$scope.AllFriends =   [{
     name: 'Preston McNeil',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/albert-einstein-profile-picture-133x133.PNG',
     location: { city: 'Houston', state: 'Texas', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 628
   },{
     name: 'Ryan Rasmussen',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/bruce-lee-profile-picture-133x133.PNG',
     location: { city: 'New York', state: 'New York', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 994
   },{
     name: 'Terri Ruff',
@@ -29,7 +24,7 @@ $scope.friends = [{
     name: 'Lindsey Mayer',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/alf-profile-picture-133x133.PNG',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 870
   },{
     name: 'Peter John Renslow',
@@ -41,7 +36,7 @@ $scope.friends = [{
     name: 'Craig Carroll',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/magnum-pi-profile-picture-133x133.PNG',
     location: { city: 'Tempe', state: 'Arizona', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 249
   },{
     name: 'Jesse Morrison',
@@ -53,55 +48,55 @@ $scope.friends = [{
     name: 'Michelle Key',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/mr-acid-man-profile-picture-133x133.PNG',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 928
   },{
     name: 'Abe Itty',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01252014/images/leo-dicaprio-profile-picture-133x133.PNG',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 1427
   },{
     name: 'Ken Peng',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01252014/images/fighter-jet-profile-picture-133x133.PNG',
     location: { city: 'Tucson', state: 'Arizona', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 376
   },{
     name: 'John Dohyung Kwon',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01262014/images/walking-dead-rick-profile-picture-133x133.PNG',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 79
   },{
     name: 'Dan Sullivan',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/eagle-profile-picture-133x133.PNG',
     location: { city: 'Cambridge', state: 'Massachusetts', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 527
   },{
     name: 'Chad Bennett',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01262014/images/wolf-profile-picture-133x133.PNG',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 1885
   },{
     name: 'Kirk Hill',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/furby-profile-picture-133x133.PNG',
     location: { city: 'Pearland', state: 'Texas', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 185
   },{
     name: 'Joseph Moses Craven',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01232014/images/mr-bean-profile-picture-133x133.PNG',
     location: { city: 'Ardmore', state: 'Oklahoma', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 708
   },{
     name: 'Jeremy James',
     pic_square: 'http://free-profile-pics.com/images/free-blackberry-bbm-profile-picture_246.png',
     location: { city: 'Provo', state: 'Utah', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 555
   },{
     name: 'Heather Clouse',
@@ -113,13 +108,13 @@ $scope.friends = [{
     name: 'Joel Gardner',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/lamborghini-profile-picture-133x133.PNG',
     location: { city: 'Provo', state: 'Utah', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 518
   },{
     name: 'Lance Winward',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/purple-tiger-profile-picture-133x133.PNG',
     location: { city: 'American Fork', state: 'Utah', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 482
   },{
     name: 'JD Clark',
@@ -131,13 +126,13 @@ $scope.friends = [{
     name: 'Spencer Mooso',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/sponge-bob-profile-picture-133x133.PNG',
     location: { city: 'Lehi', state: 'Utah', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 579
   },{
     name: 'Andrew Wiggins',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01252014/images/fighter-jet-profile-picture-133x133.PNG',
     location: { city: 'Salt Lake City', state: 'Utah', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 658
   },{
     name: 'Brittany Brown',
@@ -155,35 +150,30 @@ $scope.friends = [{
     name: 'Jeffrey Swindle',
     pic_square: 'http://free-profile-pics.com/profile-pictures/01242014/images/pumba-profile-picture-133x133.PNG',
     location: { city: 'Ann Arbor', state: 'Michigan', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: 1167
   },{
     name: 'Travis Epperson',
     pic_square: 'http://free-profile-pics.com/images/bbm_avatar_107.png',
     location: { city: 'Phoenix', state: 'Arizona', country: 'United States', zip: '' },
-    status: null,
+    status: "Whats going on?",
     friend_count: null
   },{
     name: 'Matt LeGare',
     pic_square: 'http://free-profile-pics.com/images/bbm_avatar_117.png',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 350
   },{
     name: 'Karla Jensen Pratt',
     pic_square: 'http://free-profile-pics.com/images/free-blackberry-bbm-profile-picture_175.png',
     location: null,
-    status: null,
+    status: "Whats going on?",
     friend_count: 855
-  }]
+ }];
+ $scope.sortProp;
+ $scope.sortDirection;
 
+//   //   //  //  //  //  //  //
 
-
-
-
-
-
-
-
-
-})
+});
